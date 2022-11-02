1e2f95d2e3c087b8318a319238c257e149d278f3
Owen Seeber <owenseeber@gmail.com>
2022-11-02 12:10:45 -0400

    hopefully fixed infinite loop problem

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -44,7 +44,6 @@ void TrafficLight::waitForGreen()
     // Once it receives TrafficLightPhase::green, the method returns.
 
     while(true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
         TrafficLightPhase currPhase = _msgqueue.receive();
         if (currPhase == TrafficLightPhase::green) {
             return;
@@ -71,7 +70,7 @@ void TrafficLight::cycleThroughPhases()
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
 
-    srand((unsigned) time(NULL)); // providing seed value
+    srand(time(NULL)); // providing seed value
     double cycleDuration = (4000 + (rand() % 2000)); //generating random number between 4 and 6
 
     std::chrono::time_point<std::chrono::system_clock> lastUpdate;
@@ -90,8 +89,8 @@ void TrafficLight::cycleThroughPhases()
                 _currentPhase = TrafficLightPhase::green;
             }
             _msgqueue.send(std::move(_currentPhase));
+            cycleDuration = (4000 + (rand() % 2000));
+            lastUpdate = std::chrono::system_clock::now();
         }
-        cycleDuration = (4000 + (rand() % 2000));
-        lastUpdate = std::chrono::system_clock::now();
     }
 }
